ffb4874fc232608c47b9fdd1a0af0fa704860266
jamesnitz <jamesanitz2@gmail.com>
2020-05-17 14:26:19 -0500

    hardcoded string

--- PackBuddy/Startup.cs
+++ PackBuddy/Startup.cs
@@ -28,9 +28,8 @@ namespace PackBuddy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("azureConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(this.Configuration.GetConnectionString("pack-buddy-sql")));
+
             services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
